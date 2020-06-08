b8a142ef40bfe09a5763403b18d2c1960c59debe
Zhangyi Hu <hu.zhangyi@gmail.com>
2020-06-07 20:54:31 -0400

    GraphGenerator

--- stringchain/generator.py
+++ stringchain/generator.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass
-from typing import Dict, Set
+from typing import Dict, Set, Iterable
 
 
 @dataclass
@@ -35,7 +35,7 @@ class StringGraph:
     # add vertex, child
     if child in self._info:
       if self._info[child].is_variable != variable:
-        raise ValueError(f"child node already exist with contradicting variable status {not variable}")
+        raise ValueError(f"child node {child} already exist with contradicting variable status {not variable}")
     else:
       self._info[child] = NodeInfo(set(), variable)
 
@@ -54,3 +54,52 @@ class StringGraph:
 
   def get_roots(self) -> Set[str]:
     return self._roots
+
+
+@dataclass
+class GraphGenerator:
+  var_marks = ("{", "}")
+  delimiter: str = "."
+
+  def __post_init__(self):
+    self._mark_size_left, self._mark_size_right = map(len, self.var_marks)
+    if self._mark_size_left < 1 or self._mark_size_right < 1:
+      raise ValueError("Variable marks cannot be empty")
+
+  def _node_name_and_variable_status(self, item: str):
+    match_left, match_right = item.startswith(self.var_marks[0]), item.endswith(self.var_marks[1])
+    if match_left != match_right:
+      raise ValueError(f"{item} matches variable mark on the left but not right or vice versa")
+
+    status = match_left and match_right
+    name = item[self._mark_size_left:-self._mark_size_right] if status else item
+    return status, name
+
+  def _split(self, s: str):
+    return filter(len, map(str.strip, s.split(self.delimiter)))
+
+  def _generate(self, lines: Iterable[Iterable[str]]) -> StringGraph:
+    sg = StringGraph()
+    for line in lines:
+      if line:  # if line is not empty
+        # add root
+        head, *tail = line
+        variable, name = self._node_name_and_variable_status(head)
+        if variable:
+          raise ValueError("Starting node is root, cannot be a variable")
+        sg.add_root(name)
+
+        # add the rest
+        parent = name
+        while tail:
+          head, *tail = tail
+          variable, child = self._node_name_and_variable_status(head)
+          sg.add_child(parent, child, variable)
+          parent = child
+
+    return sg
+
+  def generate(self, inputs: str) -> StringGraph:
+    lines = filter(len, map(str.strip, inputs.splitlines()))
+    strings = map(self._split, lines)
+    return self._generate(strings)
--- test/generator_test.py
+++ test/generator_test.py
@@ -1,4 +1,51 @@
-from stringchain.generator import StringGraph
+from pytest import raises
+
+from stringchain.generator import StringGraph, GraphGenerator
+
+
+def test_graph_generator():
+  inputs = """
+  foo  . bar
+
+     bar .bar.goo
+  foo.one.{two}.this
+  bar.one.this.that
+  """
+  gg = GraphGenerator()
+  sg = gg.generate(inputs)
+
+  assert sg.is_variable('two')
+  assert sg.get_roots() == {'foo', 'bar'}
+  assert sg.get_children('foo') == {'bar', 'one'}
+  assert sg.get_children('bar') == {'bar', 'goo', 'one'}
+  assert sg.get_children('one') == {'two', 'this'}
+  assert sg.get_children('this') == {'that'}
+
+
+def test_graph_generator_failure():
+  gg = GraphGenerator()
+  inputs = """
+  {foo}.bar
+  """
+  with raises(ValueError) as ex:
+    gg.generate(inputs)
+  assert "Starting node is root, cannot be a variable" == str(ex.value)
+
+  inputs = """
+  foo.bar
+  goo.{bar}
+  """
+  with raises(ValueError) as ex:
+    gg.generate(inputs)
+  assert "child node bar already exist with contradicting variable status False" == str(ex.value)
+
+  inputs = """
+  foo.{bar}
+  bar.foo
+  """
+  with raises(ValueError) as ex:
+    gg.generate(inputs)
+  assert "bar already defined as a variable, cannot be used as a root." == str(ex.value)
 
 
 def test_string_graph():
