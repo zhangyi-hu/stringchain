27d11d3c3acd979133d2bb431929cc187fa91256
Zhangyi Hu <hu.zhangyi@gmail.com>
2020-06-11 00:39:39 -0400

    conda build recipe

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+  name='stringchain',
+  version='0.8',
+  python_requires='>=3.7',
+  packages=['stringchain'],
+  url='https://github.com/zhangyi-hu/stringchain',
+  license='BSD',
+  author='Zhangyi Hu',
+  author_email='hu.zhangyi@gmail.com',
+  description=('Grammartize a given set of delimited string chains. '
+               'Eliminate typo or invalid combinations when preparing such values.')
+)
